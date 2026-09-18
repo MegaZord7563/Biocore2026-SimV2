2f1f1ecb583ddf096e5beb517b1a49f639126937
Kaique <kaiquedepaula666@gmail.com>
2026-09-17 21:42:16 -0300

    feat: implement FlywheelIO interface with input/output classes for flywheel control

--- src/main/java/br/megazord/frc7563/subsystems/Flywheel/FlywheelIO.java
+++ src/main/java/br/megazord/frc7563/subsystems/Flywheel/FlywheelIO.java
@@ -4,5 +4,46 @@
 
 package br.megazord.frc7563.subsystems.Flywheel;
 
+import org.littletonrobotics.junction.AutoLog;
+
 /** Add your docs here. */
-public interface FlywheelIO {}
+public interface FlywheelIO {
+
+    @AutoLog
+    public class FlywheelIOInputs {
+        public boolean leaderConnected = false;
+        public double leaderPositionRads = 0.0;
+        public double leaderVelocityRadsPerSec = 0.0;
+        public double leaderAppliedVoltage = 0.0;
+        public double leaderSupplyVoltage = 0.0;
+        public double leaderSupplyCurrentAmps = 0.0;
+        public double leaderTempCelsius = 0.0;
+
+        public boolean followerConnected = false;
+        public double followerPositionRads = 0.0;
+        public double followerVelocityRadsPerSec = 0.0;
+        public double followerAppliedVoltage = 0.0;
+        public double followerSupplyVoltage = 0.0;
+        public double followerSupplyCurrentAmps = 0.0;
+        public double followerTempCelsius = 0.0;
+    }
+
+    public static enum FlywheelIOOutputMode {
+        COAST,
+        BRAKE,
+        VELOCITY,
+        VOLTAGE,
+        CHARACTERIZE
+    }
+
+    public class FlywheelIOOutputs {
+        public FlywheelIOOutputMode mode = FlywheelIOOutputMode.COAST;
+        public double velocityRadsPerSec = 0.0;
+        public double voltage = 0.0;
+
+    }
+
+    public default void updateInputs(FlywheelIOInputs inputs) {}
+
+    public default void applyOutputs(FlywheelIOOutputs outputs) {}
+}
--- src/main/java/br/megazord/frc7563/subsystems/Flywheel/FlywheelIOSim.java
+++ src/main/java/br/megazord/frc7563/subsystems/Flywheel/FlywheelIOSim.java
@@ -1,5 +1,26 @@
 package br.megazord.frc7563.subsystems.Flywheel;
 
-public class FlywheelIOSim {
-    
+import br.megazord.frc7563.Constants.ModuleConstants;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class FlywheelIOSim implements FlywheelIO {
+    private static final DCMotor motorModel = DCMotor.getKrakenX60Foc(1);
+
+    private final DCMotorSim leaderMotor = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(motorModel, 0.025, ModuleConstants.kDriveMotorGearRatio),
+            motorModel);
+
+    private final DCMotorSim followerMotor = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(motorModel, 0.025, ModuleConstants.kDriveMotorGearRatio),
+            motorModel);
+
+    public FlywheelIOSim(){}
+
+    @Override
+    public void updateInputs(FlywheelIOInputs inputs){}
+
+    @Override
+    public void applyOutputs(FlywheelIOOutputs outputs){}
 }
