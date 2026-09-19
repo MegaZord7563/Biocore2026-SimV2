0601c7cc56821f143ca5b8c53e676bb27b18adff
Kaique <kaiquedepaula666@gmail.com>
2026-09-18 22:58:11 -0300

    feat: enhance TurretIO interface with input/output classes and enums for turret control

--- src/main/java/br/megazord/frc7563/subsystems/shooter/turret/TurretIO.java
+++ src/main/java/br/megazord/frc7563/subsystems/shooter/turret/TurretIO.java
@@ -2,17 +2,36 @@ package br.megazord.frc7563.subsystems.shooter.turret;
 
 import org.littletonrobotics.junction.AutoLog;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public interface TurretIO {
 
     @AutoLog
     public class TurretIOInputs
     {
+        public boolean turretConnected = false;
+        public double turretPositionRads = 0.0;
+        public double turretVelocityRadsPerSec = 0.0;
+        public double turretAppliedVoltage = 0.0;
+        public double turretSupplyVoltage = 0.0;
+        public double turretSupplyCurrentAmps = 0.0;
+        public double turretTempCelsius = 0.0;
+        public Rotation2d turretRotation2d = Rotation2d.kZero;
+    }
 
+    public enum TurretIOMode
+    {
+        POSITION,
+        VOLTAGE,
+        BREAK,
+        COAST;
     }
 
     public class TurretIOOutputs
     {
-
+        public TurretIOMode mode = TurretIOMode.COAST;
+        public Rotation2d targetRotation = Rotation2d.kZero;
+        public double voltageOut = 0.0;
     }
 
     public default void updateInputs(TurretIOInputs inputs) {}
